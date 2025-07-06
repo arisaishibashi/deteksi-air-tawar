357399515f8f779be15aed4977a37801b43b94ee
arisaishibashi <128614047+arisaishibashi@users.noreply.github.com>
2025-07-06 22:44:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ set_background("image/background.png", "image/sidebar.png")
 
 # Load model CNN
 def load_model():
-    return tf.keras.models.load_model("model_paling_baru (2).h5")
+    return tf.keras.models.load_model("model_paling_baruu.h5")
 
 model = load_model()
 
